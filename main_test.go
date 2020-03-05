e625a8281159ec3d0e08f64bb4ee926ee845e1cb
koooyooo <jab.cross.hook@gmail.com>
2020-03-06 08:50:47 +0900

    add test cases

--- main_test.go
+++ main_test.go
@@ -28,7 +28,7 @@ func TestHttpBin(t *testing.T) {
 	resps.AssertBody(t, []string{"headers/X-Amzn-Trace-Id", "headers/Hello", "origin"})
 }
 
-// 同じ実装の場合テストが成功する
+// 全く同じレスポンスの場合テストが成功する
 func TestBasic(t *testing.T) {
 	// 同じ結果を返すテストサーバを作成
 	var sampleHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
@@ -62,7 +62,7 @@ func TestBasic(t *testing.T) {
 	assert.Equal(t, bodyPair.First, bodyPair.Second)
 }
 
-// 同改装のBodyJSONの順序が異なってもテストが成功する
+// レスポンスのBodyJSON内が異なっていても、同階層の順序入れ替えのみであればテストが成功する
 func TestBasicWithDifferentJSONOrder(t *testing.T) {
 	// JSONの同列要素を入れ替えたBodyを用意
 	var sampleHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
@@ -100,7 +100,46 @@ func TestBasicWithDifferentJSONOrder(t *testing.T) {
 	assert.Equal(t, bodyPair.First, bodyPair.Second)
 }
 
-// Headerが別実装の場合 Headerの比較で失敗する
+// Statusが別の場合 Statusの比較で失敗する
+func TestDifferentStatus(t *testing.T) {
+	// Headerで別の結果を返すテストサーバを作成
+	var sampleHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/path1":
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+			w.Write([]byte(`{"headers": {"hello": "world"}, "body": {"foo": "bar"}}`))
+		case "/path2":
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(201)
+			w.Write([]byte(`{"headers": {"hello": "world"}, "body": {"foo": "bar"}}`))
+		}
+	}
+	server := httptest.NewServer(sampleHandler)
+	defer server.Close()
+
+	req1 := &model.Request{"GET", server.URL + "/path1", nil, []byte{}}
+	req2 := &model.Request{"GET", server.URL + "/path2", nil, []byte{}}
+
+	// クライアントコール
+	resps, err := executor.CallAPIs(req1, req2)
+	assert.NoError(t, err, "fails in client call")
+
+	// 結果確認
+	statusPair := resps.StatusPair()
+	// Statusが一致しない
+	assert.NotEqual(t, statusPair.First, statusPair.Second)
+
+	headerPair, err := resps.HeaderPair(nil)
+	assert.NoError(t, err)
+	assert.Equal(t, headerPair.First, headerPair.Second)
+
+	bodyPair, err := resps.BodyPair(nil)
+	assert.NoError(t, err)
+	assert.Equal(t, bodyPair.First, bodyPair.Second)
+}
+
+// Header値が別の場合 Headerの比較で失敗する
 func TestDifferentHeader(t *testing.T) {
 	// Headerで別の結果を返すテストサーバを作成
 	var sampleHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
@@ -141,7 +180,7 @@ func TestDifferentHeader(t *testing.T) {
 	assert.Equal(t, bodyPair.First, bodyPair.Second)
 }
 
-// Headerが別実装の場合 Headerの比較で失敗する
+// Header値が別の場合でも 当該属性がSkip指定していればテストは成功する
 func TestDifferentHeaderWithSkip(t *testing.T) {
 	// Headerで別の結果を返すテストサーバを作成
 	var sampleHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
